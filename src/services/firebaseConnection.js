e902670bb4ce3fd1f9043106e22302a0413c9f46
GuiManfrin134 <manfrin11gui@gmail.com>
2022-01-21 19:35:07 -0300

    Base dos Arquivos do Site

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBnF4g7T755vQiGAmphk-Eike_5L_B_R5M",
+    authDomain: "sistema-ecb8b.firebaseapp.com",
+    projectId: "sistema-ecb8b",
+    storageBucket: "sistema-ecb8b.appspot.com",
+    messagingSenderId: "847551878457",
+    appId: "1:847551878457:web:00e4f16427a1ee427d84e8",
+    measurementId: "G-ELHW21TYHW"
+};
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+  
+export default firebase;
