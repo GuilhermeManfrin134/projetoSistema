cad408794e9adbe6ca06b73cd212fd85fab6a15d
GuiManfrin134 <manfrin11gui@gmail.com>
2022-01-26 18:41:18 -0300

    Criada a tela de Login

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './signin.css';
+import logo from '../../assets/logo.png';
+
 function Signin() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e){
+      e.preventDefault();
+      alert('Clicou!')
+    }
+
     return (
-      <div>
-        <h1>Tela de Login</h1>
+      <div className='container-center'>
+        <div className='login'>
+          <div className='logo-area'>
+            <img src={logo} alt='Logo do Sistema'/>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Entrar</h1>
+            <input type='text' placeholder='nome@email.com' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+            <input type='password' placeholder='*********' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type='submit'>Acessar</button>
+          </form>
+
+          <Link to="/register">Criar uma conta</Link>
+        </div>
       </div>
     );
   }
