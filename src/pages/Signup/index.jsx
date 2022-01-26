5bd391c691b30d3d4a29da167f8ecfe36c73e633
GuiManfrin134 <manfrin11gui@gmail.com>
2022-01-26 18:52:58 -0300

    Criada a tela de Cadastro

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+import logo from '../../assets/logo.png';
+
 function Signup() {
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e){
+      e.preventDefault();
+      alert('Clicou!')
+    }
+
     return (
-      <div>
-        <h1>Tela de Cadastro</h1>
+      <div className='container-center'>
+        <div className='login'>
+          <div className='logo-area'>
+            <img src={logo} alt='Logo do Sistema'/>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Cadastrar uma conta</h1>
+            <input type='text' placeholder='Seu nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
+            <input type='text' placeholder='nome@email.com' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+            <input type='password' placeholder='*********' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type='submit'>Cadastar</button>
+          </form>
+
+          <Link to="/">Já possui uma conta?</Link>
+        </div>
       </div>
     );
   }
