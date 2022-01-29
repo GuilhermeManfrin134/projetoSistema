097b535813ec06bafcb8a813cb405b541c8e06d4
GuiManfrin134 <manfrin11gui@gmail.com>
2022-01-29 15:57:25 -0300

    Página Cliente criada

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import SignIn from '../pages/Signin';
 import SignUp from '../pages/Signup';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Customers from '../components/Customers';
 
 export default function Rout(){
     return(
@@ -14,6 +15,7 @@ export default function Rout(){
 
             <Routex exact path="/dashboard" component={Dashboard} isPrivate/>
             <Routex exact path="/profile" component={Profile} isPrivate/>
+            <Routex exact path="/customers" component={Customers} isPrivate/>
         </Switch>
     )
 }
